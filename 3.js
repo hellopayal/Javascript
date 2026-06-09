31cf449b650464cd3016fda8d031be3cf97877f1
PylPujara <137327538+PylPujara@users.noreply.github.com>
2026-06-09 15:57:11 -0700

    3. Best Time to Buy and Sell Stock

---
+++ 3.js
@@ -0,0 +1,19 @@
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+function maxProfit(prices) {
+    let minPrice = Infinity; //1
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) { //1<7
+            minPrice = prices[i]  //min = 1
+        } else {
+            maxProfit = Math.max(maxProfit , prices[i] -minPrice)
+        }
+    }
+    return maxProfit;
+
+}
+console.log(maxProfit([7,1,5,3,6,4]))
