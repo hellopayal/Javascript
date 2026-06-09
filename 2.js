6196feffbe8b560578bfa5a36cf4d6074637cd0d
PylPujara <137327538+PylPujara@users.noreply.github.com>
2026-06-09 15:08:25 -0700

    2. Two Sum

---
+++ 2.js
@@ -0,0 +1,17 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+
+}
+let nums = [2, 7, 11, 15]
+let target = 9
+console.log(twoSum(nums, target))
