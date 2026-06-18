4fbacf37a44bed2f8bbc2a1e221ae7132626a50d
PylPujara <137327538+PylPujara@users.noreply.github.com>
2026-06-18 16:35:58 -0700

    22. Valid Parentheses

---
+++ 22.js
@@ -0,0 +1,40 @@
+// Valid Parentheses
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+var isValid = function(s) {
+    const stack = []
+
+    const map = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    }
+    
+    for(let char of s){ // (]
+        if(char === "(" || char ==='{' || char === "["){
+            stack.push(char);
+        } else{
+            if(stack.pop()!== map[char]){  
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+let s = "()[]{}"
+console.log(isValid(s))
